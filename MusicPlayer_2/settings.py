44cd66fd397f9a7780dc4a3eeadaa8739b580432
tsvtln <tsvtln@abv.bg>
2024-10-27 00:37:10 +0300

    REF: refactored duplicated html into the base

--- MusicPlayer_2/settings.py
+++ MusicPlayer_2/settings.py
@@ -99,7 +99,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
-    BASE_DIR / 'staticfiles',
+    BASE_DIR / 'static',
 )
 
 MEDIA_URL = 'media/'
